0242876d7c9ed13babc795096f64a80ba8729cfe
hibbatulhakqi <hibbatulhakqi@gmail.com>
2024-12-21 16:34:42 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,40 @@
+document.writeln("<body bgcolor=yellow>");
+document.writeln("<hr color=black>");
+document.writeln("<CENTER>");
+document.writeln(
+  "<p><h1>BIODATA <q><font color=black> Fauzan Hibbatul Hakqi</font></q></h1></p>"
+);
+document.writeln("<hr color=black>");
+document.writeln("<img src=gambar3.jpg width=250px height=250px");
+document.writeln("</CENTER>");
+
+document.writeln("<div align=left>");
+document.writeln("<ol>");
+document.writeln("<li> Nim: 241011700071</li>");
+document.writeln("<li> Name: Fauzan Hibbatul Hakqi</li>");
+document.writeln("<li> Kelas: 01SIFP002</li>");
+document.writeln("</ol>");
+document.writeln("</div>");
+
+document.writeln("<div align=left>");
+document.writeln("<ul>");
+document.writeln("<li>Program Studi: Sistem Informasi</li>");
+document.writeln(
+  "<li>Alamat: Jl.Pekayon1 Rt.001/010 Pasar minggu Ragunan No.07</li>"
+);
+document.writeln("<li>Asal Sekolah: SMK PEMBANGUNAN JAYA-YAKAPI</li>");
+document.writeln("</ul>");
+document.writeln("</div>");
+
+document.writeln("<div align=left>");
+document.writeln("<ol type=1>");
+document.writeln("<li> Jurusan: Teknik Komputer Jaringan</li>");
+document.writeln("<li> Email: hibbatulhakqi@gmail.com</li>");
+document.writeln("<li> Telepon: 0856-9725-9682</li>");
+document.writeln("</ol>");
+document.writeln("</div>");
+
+document.writeln("<hr color=black>");
+document.write("<center>");
+document.writeln("Design by fauzan &copy; 20000 Allrigt Reserved");
+document.writeln("<hr color=black>");
